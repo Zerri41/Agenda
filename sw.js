d7d10ed38a8d2cee3e0b15c53b80312632a1f3b6
Zerri41 <josribeir41@gmail.com>
2026-09-16 14:57:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='agenda-v30';
+const CACHE='agenda-v33';
 const FICHEIROS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FICHEIROS)).then(()=>self.skipWaiting()));
